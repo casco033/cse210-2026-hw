82280b6f5e5bbf690fa927713da284d5432231f3
Benjamin Casco <casco033@gmail.com>
2026-09-04 21:35:01 -0600

    completed the core requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+     {
+        // Console.Write("What is the magic number? ");
+        // int number = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != randomNumber)
+        {
+            Console.Write("Guess the number I'm thinking... ");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+        
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            
+
+        }
+
     }
 }
